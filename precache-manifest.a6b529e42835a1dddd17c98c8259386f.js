b6ea73de62341a99563e94b660794e493531c5f3
mkery <mkery@cs.cmu.edu>
2020-04-30 14:49:54 -0400

    Updates

---
+++ precache-manifest.a6b529e42835a1dddd17c98c8259386f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c083f63529427b94154ae141ae764067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2462a4d8ba1332e08f68",
+    "url": "/static/css/main.6c5e8a4a.chunk.css"
+  },
+  {
+    "revision": "f17ed6e829f89338df68",
+    "url": "/static/js/2.c3883277.chunk.js"
+  },
+  {
+    "revision": "2462a4d8ba1332e08f68",
+    "url": "/static/js/main.eb61ad2c.chunk.js"
+  },
+  {
+    "revision": "8bf65ba98c0bf7854683",
+    "url": "/static/js/runtime-main.9181d3c2.js"
+  },
+  {
+    "revision": "49638aa156f519f11b7204f025c7e094",
+    "url": "/static/media/NARA.49638aa1.jpg"
+  },
+  {
+    "revision": "29a10eab4ce69d4fcc0e4fdbf3254971",
+    "url": "/static/media/mbPortrait.29a10eab.jpg"
+  }
+]);
