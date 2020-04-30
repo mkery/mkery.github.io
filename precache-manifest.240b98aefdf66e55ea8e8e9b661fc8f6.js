989db58aaad21ccb2d5e7114917514594f5cfad7
mkery <mkery@cs.cmu.edu>
2020-04-30 15:56:04 -0400

    Updates

---
+++ precache-manifest.240b98aefdf66e55ea8e8e9b661fc8f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff498f0631eab82f6c965a336a17197",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dafb83e8ed43079fb4b",
+    "url": "/static/css/main.bf4bd58b.chunk.css"
+  },
+  {
+    "revision": "7bd15a514ac570cca905",
+    "url": "/static/js/2.42ef3ce5.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.42ef3ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dafb83e8ed43079fb4b",
+    "url": "/static/js/main.88ac9281.chunk.js"
+  },
+  {
+    "revision": "71f17d60a48edfa300d7",
+    "url": "/static/js/runtime-main.b7d46f45.js"
+  },
+  {
+    "revision": "49638aa156f519f11b7204f025c7e094",
+    "url": "/static/media/NARA.49638aa1.jpg"
+  },
+  {
+    "revision": "29a10eab4ce69d4fcc0e4fdbf3254971",
+    "url": "/static/media/mbPortrait.29a10eab.jpg"
+  }
+]);
