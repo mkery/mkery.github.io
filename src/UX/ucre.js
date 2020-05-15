b1178f4bea3c6c154e7404c291e0bf247de2e553
mkery <mkery@cs.cmu.edu>
2020-05-15 19:02:13 -0400

    added new UX section

--- src/UX/ucre.js
+++ src/UX/ucre.js
@@ -1,4 +1,10 @@
 import React, { Component } from "react";
+import stakeholderMap from "./stakeholders-art.jpg";
+import interviewMural from "./Interview_Data_Affinity_Diagram.jpg";
+import reframingImg from "./Reframing.png";
+import crazy8 from "./Crazy8.png";
+import speedDate1 from "./SpeedDating1.png";
+import speedDate2 from "./SpeedDating2.png";
 
 export default class UCREPage extends Component {
   render() {
@@ -33,7 +39,11 @@ export default class UCREPage extends Component {
               monuments, murals, and historic buildings.
             </span>
             <h3>Outcome:</h3>
-            <span>User research</span>
+            <span>
+              New ideas for community programs to encourage members of the
+              public to create and show their own "amateur" or kids art
+              alongside formal public art pieces.
+            </span>
             <h3>Role:</h3>
             <span>Lead UX Designer</span>
           </div>
@@ -86,20 +96,110 @@ export default class UCREPage extends Component {
               several prominent art sites in Pittsburgh.
               <br></br>
               <br></br>
-              <span>TODO stakeholder map here</span>
+              <div className="imageFrame">
+                <img
+                  src={stakeholderMap}
+                  alt="stakeholder map for public arts in Pittsburgh"
+                />
+                <div className="imageCaption">
+                  Stakeholder Map for public arts in Pittsburgh based on my
+                  secondary research and analysis of data the from Metro 21
+                  project.
+                </div>
+              </div>
+              This research revealed few existing connections from arts
+              infrastructure to members of the public. The attempt of this prior
+              project to connect to the public through digital content
+              experienced very low usage or adoption. Thus, our next step was to
+              expand our ideas on other ways that the local community might
+              interact with art.
             </span>
 
             <h3>Primary Research</h3>
-            <span>Todo: interview mural board, then speed dating</span>
+            <span>
+              I interviewed 3 community members who somehow were involved in
+              arts in their town. Two interviewees were artists themselves, and
+              two lead organization of community arts programs for their work.
+              <div className="imageFrame">
+                <img
+                  src={interviewMural}
+                  alt="affinity diagram of interview data from artists/art community facilitators"
+                />
+                <div className="imageCaption">
+                  Affinity diagram of my analysis of interview data from artists
+                  and art community facilitators.
+                </div>
+              </div>
+              This research revealed that one successful way to engage the
+              public with local arts is to organize activities for local kids
+              and non-artist citizens to create art themselves, and then show
+              their art alongside the formal art of local artists or
+              (potentially) public arts pieces.
+            </span>
           </div>
         </div>
 
         <div className="row ux">
           <div className="row-left">
-            <h2>Insights & Outcome</h2>
+            <h2>Ideation</h2>
           </div>
           <div className="row-right">
-            <span>nsdkjfnsdkjfnsdjkfnsdkj</span>
+            <span>
+              In this phase, I sought to explore ways to reframe the problem
+              space as well as sketch initial ideas. The goal of this phase was
+              to go as creative and broad as possible to explore what might be
+              possible.
+              <div className="imageFrame">
+                <img src={reframingImg} alt="Worst ideas reframing exercise" />
+                <div className="imageCaption">
+                  <a href="https://www.interaction-design.org/literature/topics/worst-possible-idea">
+                    "Worst Possible Idea Method"
+                  </a>{" "}
+                  design exercise to explore ways to reframe the problem space.
+                </div>
+              </div>
+              <div className="imageFrame">
+                <img
+                  src={crazy8}
+                  alt="Crazy 8's design exercise sketch"
+                  style={{ width: "300px", margin: "0px calc(50% - 150px)" }}
+                />
+                <div className="imageCaption">
+                  A sketch from playing the{" "}
+                  <a href="https://designsprintkit.withgoogle.com/methodology/phase3-sketch/crazy-eights">
+                    "Crazy 8's Method"
+                  </a>{" "}
+                  rapid sketching design exercise, to quickly explore a broad
+                  space of ideas. I focused all sketches on different ways to
+                  get community members creating and displaying their art.
+                </div>
+              </div>
+            </span>
+          </div>
+        </div>
+
+        <div className="row ux">
+          <div className="row-left">
+            <h2>Speed Dating</h2>
+          </div>
+          <div className="row-right">
+            <span>
+              Moving on from the most intriguing possible solutions spaces from
+              our sketching exercises, in this design phase I aimed to probe
+              closer at understanding members of the publics' values around
+              creating, consuming, and sharing art in their community.
+            </span>
+            <div className="imageFrame">
+              <img src={speedDate1} alt="sketches from speed dating exercise" />
+              <img src={speedDate2} alt="sketches from speed dating exercise" />
+              <div className="imageCaption">
+                Two sketches I created for a speed dating study with users. Per
+                the speed dating method, the bottom image is intentionally "out
+                there" to push at users' values.
+              </div>
+            </div>
+            I ran a Speed Dating study with two participants, on 9 different
+            storyboards.
           </div>
         </div>
       </div>
