f468e2676f0c5d3a877d68f10a87d18a0abc6ddb
Mary Beth Kery <marybethkery@mac.myfiosgateway.com>
2025-11-16 16:14:18 -0500

    moving image

--- src/About/index.svelte
+++ src/About/index.svelte
@@ -10,7 +10,7 @@
   <div class="row-left">
     <img
       class="Bio-image"
-      src="/static/media/IMG_9441.jpeg"
+      src="public/resources/mkery.jpeg"
       alt="Me, Mary Beth, with some coffee like we're having a chat."
     />
     <div class="Bio-contact">
